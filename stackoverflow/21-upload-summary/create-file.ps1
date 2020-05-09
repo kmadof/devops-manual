f2ae77dcf272ee8e0ee8e61d6cf26650b631a740
Krzysztof Madej <krzysztof.madej@hotmail.com>
2020-05-09 06:44:03 +0200

    Changed format for SO-21

--- stackoverflow/21-upload-summary/create-file.ps1
+++ stackoverflow/21-upload-summary/create-file.ps1
@@ -12,6 +12,33 @@ Details
 * Skipped detail
     * MSGraph/DeviceAppManagement/MobileApps:
         * AnyDesk
+
+# Unordered list
+
+* Item 1
+* Item 2
+* Item 3
+    * Item 3a
+    * Item 3b
+    * Item 3c
+
+# Ordered list
+
+1. Step 1
+2. Step 2
+3. Step 3
+    1. Step 3.1
+    2. Step 3.2
+    3. Step 3.3
+
+# List in list
+
+1. Step 1
+2. Step 2
+3. Step 3
+    * Item 3a
+    * Item 3b
+    * Item 3c
 "@
 
 New-Item -Path . -Name "testsummary.md" -ItemType "file" -Value $content
