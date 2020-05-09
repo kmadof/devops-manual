cbc61ce8929e26c038a8085f182b3563b648b8fb
Krzysztof Madej <krzysztof.madej@hotmail.com>
2020-05-09 06:58:03 +0200

    Changed format SO-21

--- stackoverflow/21-upload-summary/create-file.ps1
+++ stackoverflow/21-upload-summary/create-file.ps1
@@ -9,6 +9,17 @@ Summary
 
 Details
 ======
+# Ordered list
+
+1. Step 1
+2. Step 2
+3. Step 3
+    1. Step 3.1
+    2. Step 3.2
+    3. Step 3.3
+
+Details
+======
 
 * Skipped detail
     * MSGraph/DeviceAppManagement/MobileApps:
