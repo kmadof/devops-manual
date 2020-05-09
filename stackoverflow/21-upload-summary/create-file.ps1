6c5368b258ff7dcfefaa58257f16ed95794757f0
Krzysztof Madej <krzysztof.madej@hotmail.com>
2020-05-09 06:04:52 +0200

    Added SO-21

---
+++ stackoverflow/21-upload-summary/create-file.ps1
@@ -0,0 +1,21 @@
+$content = @"
+Summary
+======
+- Created resources: 0
+- Deleted resources: 0
+- Updated resources: 0
+- Unchanged resources: 117
+- Skipped resources: 1
+
+Details
+======
+- Skipped detail
+  - MSGraph/DeviceAppManagement/MobileApps:
+    - AnyDesk
+"@
+
+New-Item -Path . -Name "testsummary.md" -ItemType "file" -Value $content
+
+Get-Location
+
+Get-ChildItem
