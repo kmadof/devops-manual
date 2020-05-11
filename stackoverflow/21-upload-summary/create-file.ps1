0c24b7265a42153132d4e81bc9003038803bd991
Krzysztof Madej <krzysztof.madej@hotmail.com>
2020-05-11 10:26:09 +0200

    Changed SO-21

--- stackoverflow/21-upload-summary/create-file.ps1
+++ stackoverflow/21-upload-summary/create-file.ps1
@@ -30,31 +30,9 @@ Details
         3. AnyDesk
 
 # Unordered list
-
-* Item 1
-* Item 2
-* Item 3
-    * Item 3a
-    * Item 3b
-    * Item 3c
-
-# Ordered list
-
-1. Step 1
-2. Step 2
-3. Step 3
-    1. Step 3.1
-    2. Step 3.2
-    3. Step 3.3
-
-# List in list
-
-1. Step 1
-2. Step 2
-3. Step 3
-    * Item 3a
-    * Item 3b
-    * Item 3c
+- Test
+    - Test 2
+        - Test3
 "@
 
 New-Item -Path . -Name "testsummary.md" -ItemType "file" -Value $content
