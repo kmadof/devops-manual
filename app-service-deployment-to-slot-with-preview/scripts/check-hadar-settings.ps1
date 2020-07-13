00d07d64f4df330e6849cb0cebb07a7bdba7ceb8
kmadof <krzysztof.madej.1987@gmail.com>
2020-07-13 15:29:21 +0200

    Added param for webappName

--- app-service-deployment-to-slot-with-preview/scripts/check-hadar-settings.ps1
+++ app-service-deployment-to-slot-with-preview/scripts/check-hadar-settings.ps1
@@ -1,6 +1,7 @@
-﻿$appName = 'Hadar'
+﻿param([String] $WebappName)
+
 $resourceGroup = 'TheCodeManual'
 $expected = 'SomeValue', 'ANCM_ADDITIONAL_ERROR_PAGE_LINK', 'AnotherValue2'
 $ignoreSettings = 'WEBSITE_RUN_FROM_PACKAGE', 'Testowo'
 
-.\check-webapp-settings.ps1 -WebappName $appName -ResourceGroup $resourceGroup -Expected $expected -IgnoreSettings $ignoreSettings
+.\check-webapp-settings.ps1 -WebappName $WebappName -ResourceGroup $resourceGroup -Expected $expected -IgnoreSettings $ignoreSettings
