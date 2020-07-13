75de70ddd7342988f201eb77fa5d90e733f48155
kmadof <krzysztof.madej.1987@gmail.com>
2020-07-13 16:14:09 +0200

    Fixed checking configuration script

--- app-service-deployment-to-slot-with-preview/scripts/check-hadar-settings.ps1
+++ app-service-deployment-to-slot-with-preview/scripts/check-hadar-settings.ps1
@@ -1,7 +1,7 @@
 ﻿param([String] $WebappName)
 
 $resourceGroup = 'TheCodeManual'
-$expected = 'SomeValue', 'ANCM_ADDITIONAL_ERROR_PAGE_LINK', 'AnotherValue2'
+$expected = 'SomeValue', 'ANCM_ADDITIONAL_ERROR_PAGE_LINK', 'AnotherValue'
 $ignoreSettings = 'WEBSITE_RUN_FROM_PACKAGE', 'Testowo'
 
 .\check-webapp-settings.ps1 -WebappName $WebappName -ResourceGroup $resourceGroup -Expected $expected -IgnoreSettings $ignoreSettings
