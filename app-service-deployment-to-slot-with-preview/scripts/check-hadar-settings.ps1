caed94c3c0e7e000034e81d31a9c593e78986d25
kmadof <krzysztof.madej.1987@gmail.com>
2020-07-13 10:04:50 +0200

    Added scripts to verify configuration settings

---
+++ app-service-deployment-to-slot-with-preview/scripts/check-hadar-settings.ps1
@@ -0,0 +1,6 @@
+﻿$appName = 'Hadar'
+$resourceGroup = 'TheCodeManual'
+$expected = 'SomeValue', 'ANCM_ADDITIONAL_ERROR_PAGE_LINK', 'AnotherValue2'
+$ignoreSettings = 'WEBSITE_RUN_FROM_PACKAGE', 'Testowo'
+
+C:\dev\scripts\check-webapp-settings.ps1 -WebappName $appName -ResourceGroup $resourceGroup -Expected $expected -IgnoreSettings $ignoreSettings
---
+++ app-service-deployment-to-slot-with-preview/scripts/check-webapp-settings.ps1
@@ -0,0 +1,13 @@
+﻿param([String] $WebappName, [String] $ResourceGroup, [String[]] $Expected, [String[]] $IgnoreSettings)
+
+$settings = az webapp config appsettings list --name $WebappName --resource-group $ResourceGroup --query '[].[name]' --out tsv
+
+$filtereddata = $settings | Where{ $_ -notin $IgnoreSettings }
+
+$result = Compare-Object -ReferenceObject  $filtereddata -DifferenceObject $Expected
+
+Write-Host $result
+
+if($result.Length -ne 0) {
+   throw "They are missing settings in webapp $($WebappName)"
+}
