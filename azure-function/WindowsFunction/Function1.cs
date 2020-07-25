06c7c56ef1c286f4b2ee573a24a46a80ad86486f
kmadof <krzysztof.madej.1987@gmail.com>
2020-07-25 12:36:12 +0200

    Hot swap test

--- azure-function/WindowsFunction/Function1.cs
+++ azure-function/WindowsFunction/Function1.cs
@@ -27,7 +27,7 @@ namespace WindowsFunction
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"Hello, mister {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
         }
