ac510cb159a21380418f6e1191c0409c4a0b834c
kmadof <krzysztof.madej.1987@gmail.com>
2020-12-01 11:02:51 +0100

    Changed SO pipeline at Tuesday 01/12/2020 11:02 +01:00 [skip ci]

--- change-pipeline.ps1
+++ change-pipeline.ps1
@@ -1,7 +1,7 @@
 git add .
 
 $date = Get-Date -Format "dddd dd/MM/yyyy HH:mm K"
-$message = "Changed SO pipeline $($date) [skip ci]"
+$message = "Changed SO pipeline at $($date) [skip ci]"
 
 git commit -m $message
 
